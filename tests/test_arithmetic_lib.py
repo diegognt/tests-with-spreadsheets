7073c0ed036d250ea2b6d053bcd1fda25f672251
Diego Navarro <code@diegognt.dev>
2024-08-02 05:53:42 -0500

    initial set up

---
+++ tests/test_arithmetic_lib.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class ArithmeticTest(unittest.TestCase):
+    def setUp(self) -> None:
+        # prepare the data, brinf the data from Google Spreadsheet
+        pass
+
+    def tests_arithmetic(self) -> None:
+        # test the arithmetic functions
+        pass
+
+    def tearDown(self) -> None:
+        # upload the data to Google Spreadsheet
+        pass
