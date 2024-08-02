8eeede88d45db7872d39f21e3c8a64e2cb920db7
Diego Navarro <code@diegognt.dev>
2024-08-02 16:09:37 -0500

    implements a data-driven tests using Google Sheets

--- tests/test_arithmetic_lib.py
+++ tests/test_arithmetic_lib.py
@@ -1,15 +1,85 @@
+import os
+from typing import Any, Dict
+import pandas as pd
 import unittest
+from utilities.google_sheets import GoogleSheetsClient
+from app.arithmetic import add, subtract, multiply, divide
 
 
 class ArithmeticTest(unittest.TestCase):
-    def setUp(self) -> None:
-        # prepare the data, brinf the data from Google Spreadsheet
-        pass
+    sheets_client = GoogleSheetsClient()
+    spreadsheet_id: str | None = ""
+    data: pd.DataFrame = []
+    results: list[Dict[str, Any]] = []
+
+    @classmethod
+    def setUp(cls) -> None:
+        cls.spreadsheet_id = os.environ.get("SPREADSHEET_ID")
+
+        if not cls.spreadsheet_id:
+            raise ValueError(
+                "SPREADSHEET_ID is not set in the environment variables."
+            )
+
+        data_range = "arithmetic_test_cases!A1:D"
+        cls.data = cls.sheets_client.fetch_data(
+            spreadsheet_id=cls.spreadsheet_id, range=data_range
+        )
 
     def tests_arithmetic(self) -> None:
-        # test the arithmetic functions
-        pass
+        for index, row in self.data.iterrows():
+            with self.subTest(
+                f"Test case {index}, with operation {row['operation']}"
+            ):
+                try:
+                    operation = row["operation"]
+                    a = int(row["a"])
+                    b = int(row["b"])
+                    expected = row["expected"]
+                    result: int | float = 0
 
-    def tearDown(self) -> None:
-        # upload the data to Google Spreadsheet
-        pass
+                    if operation == "addition":
+                        result = add(a, b)
+                    elif operation == "subtraction":
+                        result = subtract(a, b)
+                    elif operation == "multiplication":
+                        result = multiply(a, b)
+                    elif operation == "division":
+                        result = divide(a, b)
+
+                    self.assertEqual(result, int(expected))
+                    self.results.append(
+                        {
+                            "operation": operation,
+                            "a": a,
+                            "b": b,
+                            "expected": expected,
+                            "result": result,
+                            "status": "PASS",
+                        }
+                    )
+                except Exception as e:
+                    self.results.append(
+                        {
+                            "operation": operation,
+                            "a": a,
+                            "b": b,
+                            "expected": expected,
+                            "result": str(e),
+                            "status": "FAIL",
+                        }
+                    )
+
+                    if expected == "error":
+                        self.assertRaises(ValueError, divide, a, b)
+                    else:
+                        self.fail(f"Unexpected error: {e}")
+
+    @classmethod
+    def tearDown(cls) -> None:
+        df = pd.DataFrame(cls.results)
+        print(df)
+
+
+if __name__ == "__main__":
+    unittest.main()
